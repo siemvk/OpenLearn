23655ac7401986f143b5b863ac0cbf2b4faea930
andrei1010 <andrei1010@andrei1010.net>
2024-11-14 18:05:13 +0100

    Added simple footer w/ current git commit and branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { TopNavBar } from "@/components/navbar/topNavBar";
 import { headers } from "next/headers";
+import Footer from "@/components/footer/footer";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default async function RootLayout({
         >
         {!hideNavbar && <TopNavBar />}
         {children}
+        <Footer />
         </body>
         </html>
     );
