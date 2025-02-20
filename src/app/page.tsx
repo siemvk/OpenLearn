9a3b4e9118e5ab7289133b49aefab2e7daebb57f
andr3i1010 <andrei@andrei1010.me>
2025-02-20 21:22:14 +0000

    add some fire marquees to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import Button1 from '@/components/button/Button1';
 import './home.css';
+import ReviewCard from '@/components/misc/reviewCard';
+import Marquee from '@/components/misc/Marquee';
+import Image from 'next/image';
+// Subject images //
+
+import nsk_img from '@/app/img/nask.svg';
+import math_img from '@/app/img/math.svg';
+import eng_img from '@/app/img/english.svg';
+import fr_img from '@/app/img/baguette.svg';
+import de_img from '@/app/img/pretzel.svg';
+import nl_img from '@/app/img/nl.svg';
+import ak_img from '@/app/img/geography.svg';
+import ckv_img from '@/app/img/ckv.svg';
+
 export default function Home() {
     return (
         <div className="flex flex-col">
             <div className="hidden md:flex flex-col">
                 <div className="relative flex">
                     <section
-                        className="w-screen h-[calc(100vh-4rem)] bg-neutral-800 pt-8 pb-8 drop-shadow-xl drop-down">    
+                        className="w-screen h-[calc(100vh-4rem)] bg-neutral-800 pt-8 pb-8 drop-shadow-xl drop-down">
                         <div className='flex flex-row h-full'>
                             <div className="flex flex-col justify-center">
                                 <div className="flex ml-20">
@@ -14,14 +28,14 @@ export default function Home() {
                                         PolarLearn
                                     </h1>
                                 </div>
-                                <br/>
-                                <br/>
+                                <br />
+                                <br />
                                 <div className='w-1/2 items-center justify-center'>
                                     <h1 className="text-center  mt-6 flex-col sm:flex-row  text-4xl font-bold leading-tight bg-clip-text drop-down">
                                         De gratis en Open-Source leerprogramma voor al je schoolvakken
-                                        <br/>
+                                        <br />
                                     </h1>
-                                    <br/>
+                                    <br />
                                     <div className='justify-center flex'>
                                         <Button1 text="Meer weten" />
                                     </div>
@@ -35,10 +49,109 @@ export default function Home() {
                 </div>
             </div>
             <hr className="flex-grow border-neutral-600 m-3" />
+            <Marquee direction='right'>
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={nl_img} alt={"nederlands plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        Nederlands
+                    </span>
+                </div>
+                <div className='w-4' />
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={de_img} alt={"duits plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        Duits
+                    </span>
+                </div>
+                <div className='w-4' />
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={fr_img} alt={"frans plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        Frans
+                    </span>
+                </div>
+                <div className='w-4' />
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={eng_img} alt={"engels plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        Engels
+                    </span>
+                </div>
+                <div className='w-4' />
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={math_img} alt={"wiskunde plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        Wiskunde
+                    </span>
+                </div>
+                <div className='w-4' />
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={nsk_img} alt={"nask plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        NaSk
+                    </span>
+                </div>
+                <div className='w-4' />
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-40 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={ak_img} alt={"aardrijkskunde plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        Aardrijkskunde
+                    </span>
+                </div>
+                <div className='w-4' />
+                <div
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-40 h-14 text-center place-items-center grid"
+                >
+                    <span className="flex items-center">
+                        <Image src={ckv_img} alt={"ckv plaatje"} width={20} height={20} />
+                        <div className="w-2" />
+                        CKV
+                    </span>
+                </div>
+                
+            </Marquee>
+            <hr className="flex-grow border-neutral-600 m-3" />
+            <div className='flex w-full items-center justify-center flex-col'>
+                <h1 className='text-2xl font-bold'>Wat onze fans vinden:</h1>
+                <div className='h-4' />
+                <Marquee direction='left'>
+                    <div className='flex flex-row gap-x-4 w-min'>
+                        <ReviewCard stars={5} author='andrei1010' comment='Door de gratis forum en de samenvattingen heb ik eindelijk topcijfers voor geschiedenis!' />
+                        <ReviewCard stars={4.7} author='EGaming200' comment='PolarLearn heeft mij geholpen met leren voor toetsen' />
+                        <ReviewCard stars={5} author='mrapfelstrudel' comment='Door PolarLearn heb ik eindelijk goede cijfers voor frans!' />
+                        <ReviewCard stars={0} author='StudyGo Admin' comment='waarom bestaat dit' />
+                        <ReviewCard stars={5} author='anoniem' comment='Ik merk dat PolarLearn veeel sneller is dan StudyGo, ook heel fijn dat het gratis is' />
+                        <ReviewCard stars={5} author='kabab33' comment='Ik had een StudyGo abonnement, maar nu niet meer! Bedankt voor mijn € 143,88 per jaar besparen PolarLearn!' />
+                        <ReviewCard stars={5} author='luna' comment="Mijn cijfers waren eerst heel slecht, maar sinds ik PolarLearn gebruik zijn ze super hoog!" />
+                    </div>
+                </Marquee>
+            </div>
+            <hr className="flex-grow border-neutral-600 m-3" />
             <div className='h-full'>
                 <div className='w-full h-64 flex'>
                     <div className='w-1/2'>
-                    Lorem Ipsum
+                        Lorem Ipsum
                     </div>
                     <div className='border-r border-neutral-600 h-full'></div>
                     <div className='w-1/2 pl-5'>
---
+++ src/components/misc/reviewCard.tsx
@@ -0,0 +1,35 @@
+export default function ReviewCard({ stars, author, comment }: { stars: number; author: string; comment: string; }) {
+    return (
+        <>
+            <div className="flex flex-col bg-neutral-800 h-64 w-72 m-auto rounded-lg p-5">
+                <h1 className="text-2xl font-bold">
+                    {author}
+                </h1>
+                <div className="flex">
+                    {[...Array(5)].map((_, index) => {
+                        const starValue = stars - index;
+                        if (starValue >= 1) {
+                            return <span key={index} className="text-yellow-400 text-2xl">★</span>;
+                        } else if (starValue > 0) {
+                            const widthPercent = Math.round(starValue * 100);
+                            return (
+                                <span key={index} className="relative text-2xl">
+                                    <span className="text-gray-400">★</span>
+                                    <span className="absolute top-0 left-0 overflow-hidden" style={{ width: `${widthPercent}%`, color: "#facc15" }}>
+                                        ★
+                                    </span>
+                                </span>
+                            );
+                        } else {
+                            return <span key={index} className="text-gray-400 text-2xl">★</span>;
+                        }
+                    })}
+                </div>
+                <div className="h-4"/>
+                <p className="text-lg">
+                    {comment}
+                </p>
+            </div>
+        </>
+    );
+}
