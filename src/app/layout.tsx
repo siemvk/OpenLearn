cba10646a5b793a5feb5c9420d4797f443e07ba1
andr3i1010 <andrei@andrei1010.me>
2025-04-14 06:45:46 +0000

    add noscript to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,6 +117,17 @@ export default async function RootLayout({
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
       </Head>
       <body className={`antialiased flex flex-col min-h-screen `}>
+        <noscript>
+          <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black text-white text-center p-4">
+            <div className="flex flex-col items-center">
+              <p className="text-6xl pb-4">❌</p>
+
+              <p className="text-xl">
+                PolarLearn werkt niet zonder JavaScript. Zet JavaScript aan om verder te gaan.
+              </p>
+            </div>
+          </div>
+        </noscript>
         <div
           style={{ display: "none" }}
           dangerouslySetInnerHTML={{ __html: art }}
