f27bf9f0353c97058aaa13a4a5de4770b8a00b98
Siem <vankeulensiem@gmail.com>
2026-03-22 15:18:08 +0100

    sticky

--- app/components/list/list.tsx
+++ app/components/list/list.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router"
 export const ListContainer: React.FC<{ children: React.ReactNode, className: string }> = ({ children, className }) => {
     return (
-        <div className={"flex flex-col gap-2 rounded-2xl p-6 w-full max-w-lg bg-openlearn-800 " + className} >
+        <div className={"flex flex-col gap-2 w-full " + className} >
             {children}
         </div>
     )
@@ -15,7 +15,7 @@ export const ListItem: React.FC<{
 }> = ({ children, linkTo, title, subtitle }) => {
     const navigate = useNavigate()
     return (
-        <div className="bg-openlearn-700 rounded-xl p-4 cursor-pointer" onClick={() => navigate(linkTo)}>
+        <div className="bg-openlearn-800 rounded-xl p-4 cursor-pointer" onClick={() => navigate(linkTo)}>
             <h1 className="font-semibold text-lg text-gray-100">{title}</h1>
             <p className="text-gray-200 text-sm">{subtitle}</p>
             {/* aan de linkerkant de rest van de dingen laten zien (voor knoppen enzo) */}
