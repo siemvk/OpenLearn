3040ba9fc9c8a030d75556ca01d1ba485612577c
Siem <vankeulensiem@gmail.com>
2026-04-11 17:36:51 +0200

    new logo's i like

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,8 @@
 import i18n from "i18next";
 import { useNavigate } from "react-router";
 import { Button } from "~/components/button/button";
-import logo from "~/../public/logo_outline.svg";
+import logo from "~/../public/logos/OL-VT-LONG-LOGO.png";
+
 
 export default function Home() {
   const navigate = useNavigate();
@@ -9,8 +10,7 @@ export default function Home() {
     <>
       <div className="flex flex-col items-center justify-center">
         <div className="flex flex-row rounded-2xl w-full md:w-auto px-6 py-4 items-center justify-center">
-          <h1 className="text-3xl md:text-3xl font-bold text-center">OpenLearn</h1>
-          <img src={logo} alt="Logo" className="ml-4 w-32" />
+          <img src={logo} alt="Logo" className="ml-4 w-128" />
         </div>
         <Button onClick={() => navigate('/auth/login')}>{i18n.t('auth:signupMarketing')}</Button>
       </div>
